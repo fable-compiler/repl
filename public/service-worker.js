4674daff5c79e15c6e48d711a979f5a453470cc0
Diego Esmerio <menhowca@gmail.com>
2018-08-06 23:47:27 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,36 @@
 var fableCache = 'fable-repl2-v1';
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.filter(function(cacheName) {
-            return cacheName !== fableCache;
-          }).map(function(cacheName) {
-            return caches.delete(cacheName);
-          })
-        );
-      })
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName !== fableCache;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
     );
-  });
+});
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.open(fableCache).then(function(cache) {
-        return cache.match(event.request).then(function (response) {
-          return response || fetch(event.request).then(function(response) {
-            var url = new URL(event.request.url);
-            if(!url.pathname.startsWith('/samples'))
-                cache.put(event.request, response.clone());
-            return response;
-          });
-        });
-      })
+        caches.open(fableCache).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                var url = new URL(event.request.url);
+                if (url.pathname.startsWith('/samples'))
+                    return fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    }, function () {
+                        return response;
+                    });
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
     );
-  });
+});
