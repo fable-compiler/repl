5b7b0376a435933ea13db464711905c5b43aa31e
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-09-23 17:08:00 +0200

    Create one file per widget

---
+++ src/App/Widgets/About.fs
@@ -0,0 +1,15 @@
+module Widgets.About
+
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fulma
+open Fable.Repl.Shared
+
+let view =
+    Content.content [ ]
+        [ str ("Version: " + Literals.VERSION)
+          br [ ]
+          a [ Href "https://github.com/fable-compiler/repl2/issues/new" ]
+            [ Text.span [ Modifiers [ Modifier.TextTransform TextTransform.Italic ]
+                          Props [ Style [ TextDecoration "underline" ] ] ]
+                [ str "Found a bug ?" ] ] ]
