6fae8e9df978a8cc16d9cb82a254cd39c9d9e29f
Eugene Tolmachev <eugene@tolmachev.ca>
2018-10-23 09:17:12 -0400

    Update todomvc.fs
    typo fix

--- public/samples/elmish/todomvc.fs
+++ public/samples/elmish/todomvc.fs
@@ -1,7 +1,7 @@
 module Elmish.TodoMVC
 
 (**
- TodoMVC app ported to Elm.
+ TodoMVC app ported from Elm.
  You can find more info about Emish architecture and samples at https://elmish.github.io/
  NOTE: The API in Fable's REPL may differ from Fable.Elmish & Fable.React nuget libraries.
        The generated JS code won't be as optimized as when using dotnet-fable.
