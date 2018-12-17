e9330537855a1b1d73b5dc1bd03f28b97e524f10
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2018-12-17 14:08:45 +0100

    Remove debug printing

--- src/App/Editor.fs
+++ src/App/Editor.fs
@@ -46,9 +46,7 @@ let createCompletionProvider getCompletions =
         member __.provideCompletionItems(model, position, _context, _token) =
             async {
                 let lineText = model.getLineContent(position.lineNumber)
-                printfn "Get completion for L%i-%i: %s" position.lineNumber position.column lineText
                 let! completions = getCompletions position.lineNumber position.column lineText
-                printfn "Found completions: %i" (Array.length completions)
                 return
                     completions
                     |> Array.map (fun (c: Fable.Repl.Completion) ->
