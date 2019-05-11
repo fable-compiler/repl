92afa0dc15ff465a970e0f0b24eca8904e74d887
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2019-05-11 23:52:40 +0200

    Updates

--- samples/fulma/icon.fs
+++ samples/fulma/icon.fs
@@ -5,174 +5,142 @@ open Fable.React
 open Fable.React.Props
 open Fulma
 
-let icon () =
-    div [ Class "block" ]
-        [ Icon.icon [ Icon.Size IsSmall ]
-            [ i [ Class "fa fa-home" ] [ ] ]
-          Icon.icon [ ]
-            [ i [ Class "fa fa-lg fa-home" ] [ ] ]
-          Icon.icon [ Icon.Size IsMedium ]
-            [ i [ Class "fa fa-2x fa-home" ] [ ] ]
-          Icon.icon [ Icon.Size IsLarge ]
-            [ i [ Class "fa fa-3x fa-home" ] [ ] ] ]
-
-//Display modification of container size with Bulma options
 let containerSizes () =
+  div [ Class "container-sizes" ]
+   [ div [ Class "block" ]
+        [ Icon.icon [ Icon.Size IsSmall ] // Container size
+            [ i [ Class "fas fa-home" ] [ ] ] ]
+     div [ Class "block" ]
+        [ Icon.icon [ ]
+            [ i [ Class "fas fa-home" ] [ ] ]
+          Icon.icon [ ]
+            [ i [ Class "fas fa-lg fa-home" ] [ ] ] ]
+     div [ Class "block" ]
+        [ Icon.icon [ Icon.Size IsMedium ]
+            [ i [ Class "fas fa-home" ] [ ] ]
+          Icon.icon [ Icon.Size IsMedium ]
+            [ i [ Class "fas fa-lg fa-home" ] [ ] ]
+          Icon.icon [ Icon.Size IsMedium ]
+            [ i [ Class "fas fa-2x fa-home" ] [ ] ] ]
+     div [ Class "block" ]
+        [ Icon.icon [ Icon.Size IsLarge ]
+            [ i [ Class "fas fa-home" ] [ ] ]
+          Icon.icon [ Icon.Size IsLarge ]
+            [ i [ Class "fas fa-lg fa-home" ] [ ] ]
+          Icon.icon [ Icon.Size IsLarge ]
+            [ i [ Class "fas fa-2x fa-home" ] [ ] ]
+          Icon.icon [ Icon.Size IsLarge ]
+            [ i [ Class "fas fa-3x fa-home" ] [ ] ] ]
+   ]
+
+// Diplay Font Awesome Rotation & Flip
+let iconRotationFlip () =
     div [ Class "block" ]
-        [ //faIcon creates one Fulma icon element containing one Font Awesome icon
-          Icon.icon [ // First group of Options: Usual fulma properties available: size, class
-                        // Icon container size set to small in Fulma
-                        Icon.Size IsSmall;
-                        // Custom class to set background color to yellow to display container
-                        Icon.CustomClass "icon-size" ]
-                        //Second group of Options: Font Awesome options
-                        [ //Display one Font Awesome Icon available in the Fa.I namespace
-                          i [ Class "fas fa-home" ] [] ]
-          Icon.icon [ //No setting for Icon container size = Normal size
-                        //...
-                        Icon.CustomClass "icon-size" ]
-                      [ i [ Class "fas fa-home" ] [] ]
-          Icon.icon [ Icon.Size IsMedium
-                      Icon.CustomClass "icon-size" ]
-                        //Makes the Icon 33% larger
-                    [ i [ Class "fas fa-home fa-lg" ] [] ]
-          Icon.icon [ Icon.Size IsLarge
-                      Icon.CustomClass "icon-size" ]
-                    //Makes the Icon 2x times larger
-                    [ i [ Class "fas fa-home fa-2x" ] [] ] ]
+        [ ul [ ]
+             [ li [ ]
+                  [ Icon.icon [ Icon.Size IsMedium ]
+                                [ i [ Class "fas fa-shield-alt fa-lg" ] [] ]
+                    str "No Rotation" ]
+               li [ ]
+                  [ Icon.icon [ Icon.Size IsMedium ]
+                                [ i [ Class "fas fa-shield-alt fa-lg fa-rotate-90" ] [] ]
+                    str "90 degrees rotation" ]
+               li [ ]
+                  [ Icon.icon [ Icon.Size IsMedium ]
+                                [ i [ Class "fas fa-shield-alt fa-lg fa-rotate-180" ] [] ]
+                    str "180 degrees rotation" ]
+               li [ ]
+                  [ Icon.icon [ Icon.Size IsMedium ]
+                                [ i [ Class "fas fa-shield-alt fa-lg fa-flip-horizontal" ] [] ]
+                    str "Horizontal flip" ]
+               li [ ]
+                  [ Icon.icon [ Icon.Size IsMedium ]
+                                [ i [ Class "fas fa-shield-alt fa-lg fa-flip-vertical" ] [] ]
+                    str "Vertical flip" ] ] ]
 
-//Diplay Font Awesome Rotation & Flip
-// let iconRotationFlip () =
-//     div [ Class "block" ]
-//         [ ul [ ]
-//              [ li [ ]
-//                   [ Icon.icon [ Icon.Size IsMedium ]
-//                                 [ Fa.icon Fa.I.Shield
-//                                   Fa.faLg  ]
-//                     str "No Rotation" ]
-//                li [ ]
-//                   [ Icon.icon [ Icon.Size IsMedium ]
-//                                 [ Fa.icon Fa.I.Shield
-//                                   //Rotate 90 degrees
-//                                   Fa.rotate90
-//                                   //Large icon
-//                                   Fa.faLg  ]
-//                     str "90 degrees rotation" ]
-//                li [ ]
-//                   [ Icon.icon [ Icon.Size IsMedium ]
-//                                 [ Fa.icon Fa.I.Shield
-//                                   //Rotation 180 degrees
-//                                   Fa.rotate180
-//                                   Fa.faLg ]
-//                     str "180 degrees rotation" ]
-//                li [ ]
-//                   [ Icon.icon [ Icon.Size IsMedium ]
-//                                 [ Fa.icon Fa.I.Shield
-//                                   //Flip Horizontal
-//                                   Fa.flipHorizontal
-//                                   Fa.faLg ]
-//                     str "Horizontal flip" ]
-//                li [ ]
-//                   [ Icon.icon [ Icon.Size IsMedium ]
-//                                 [ Fa.icon Fa.I.Shield
-//                                   //Flip Vertical
-//                                   Fa.flipVertical
-//                                   Fa.faLg ]
-//                     str "Vertical flip" ] ] ]
+//Display Font Awesome Animations
+let iconAnimations () =
+    div [ Class "block" ]
+        [ ul [ ]
+             [ li [ ]
+                  [ Icon.icon [ Icon.Size IsLarge ]
+                                [ //Animations work well on Spinner
+                                  //Pulse Animation
+                                  //Icon 2x times larger
+                                  i [ Class "fas fa-spinner fa-2x fa-pulse" ] [] ]
+                    str "Pulse animation" ]
+               li [ ]
+                  [ Icon.icon [ Icon.Size IsLarge ]
+                                [ //Animations work well wit Cog
+                                  //Spin animation
+                                  //Icon 2x times larger
+                                  i [ Class "fas fa-cog fa-2x fa-spin" ] [] ]
+                    str "Spin animation" ] ] ]
 
-// //Display Font Awesome Animations
-// let iconAnimations () =
-//     div [ Class "block" ]
-//         [ ul [ ]
-//              [ li [ ]
-//                   [ Icon.icon [ Icon.Size IsLarge ]
-//                                 [ //Animations work well on Spinner
-//                                   Fa.icon Fa.I.Spinner
-//                                   //Pulse Animation
-//                                   Fa.pulse
-//                                   //Icon 2x times larger
-//                                   Fa.fa2x ]
-//                     str "Pulse animation" ]
-//                li [ ]
-//                   [ Icon.icon [ Icon.Size IsLarge ]
-//                                 [ //Animations work well wit Cog
-//                                   Fa.icon Fa.I.Cog
-//                                   //Spin animation
-//                                   Fa.spin
-//                                   //Icon 2x times larger
-//                                   Fa.fa2x ]
-//                     str "Spin animation" ] ] ]
+//Stacked Icons
+let stackedIcons () =
+    div [ Class "block" ]
+        [ ul [ ]
+             [ li [ ]
+                  [ span [ Class "fa-stack fa-lg" ]
+                        [ i [ Class "far fa-square fa-stack-2x" ] []
+                          i [ Class "fab fa-twitter fa-stack-1x" ] [] ]
+                    str "Twitter logo over a square with round corners" ]
+               li [ ]
+                  [ span [ Class "fa-stack fa-lg" ]
+                        [ i [ Class "fas fa-circle fa-stack-2x" ] []
+                          i [ Class "fas fa-flag fa-stack-1x fa-inverse" ] [] ]
+                    str "One flag with inversed color over a circle" ] ] ]
 
-// //Stacked Icons
-// let stackedIcons () =
-//     div [ Class "block" ]
-//         [ ul [ ]
-//              [ li [ ]
-//                   [ Icon.stackParent [ Fa.Parent.faLg ]
-//                                      [ Icon.stackChild [ Fa.Child.faStack2x; Fa.Child.icon Fa.I.SquareO ]
-//                                        Icon.stackChild [ Fa.Child.faStack1x; Fa.Child.icon Fa.I.Twitter ] ]
-//                     str "Twitter logo over a square with round corners" ]
-//                li [ ]
-//                   [ Icon.stackParent [ Fa.Parent.faLg ]
-//                                      [ Icon.stackChild [ Fa.Child.faStack2x; Fa.Child.icon Fa.I.Circle ]
-//                                        Icon.stackChild [ Fa.Child.faStack1x; Fa.Child.icon Fa.I.Flag; Fa.Child.colorInverse ] ]
-//                     str "One flag with inversed color over a circle" ] ] ]
+//Display Font Awesome Icon List
+let iconList () =
+    div [ Class "block" ]
+        [ // fa-ul creates an unordered list with icons instead of the classic bullet points
+          ul [ Class "fa-ul" ]
+            [ li [ ]
+                [ Icon.icon [ ]
+                    [ i [ Class "fas fa-check-square"] [] ]
+                  str "Item done" ]
+              li [ ]
+                [ Icon.icon [ ]
+                    [ i [ Class "fas fa-spinner fa-spin"] [] ]
+                  str "Item processing" ] ] ]
 
-// //Display Font Awesome Icon List
-// let iconList () =
-//     div [ Class "block" ]
-//         [ // fa_ul creates an unordered list with icons instead of the classic bullet points
-//           Icon.fa_ul [ ]
-//             [ li [ ]
-//                 [ Icon.icon [ ]
-//                               [ Fa.icon Fa.I.CheckSquare
-//                                 Fa.isLi ]
-//                   str "Item done" ]
-//               li [ ]
-//                 [ Icon.icon [ ]
-//                               [ Fa.icon Fa.I.Spinner
-//                                 Fa.spin
-//                                 Fa.isLi ]
-//                   str "Item processing" ] ] ]
+let borderPulledIcons () =
+    div [ Class "block" ]
+        [ span [ ]
+               [ Icon.icon [ ]
+                    [ i [ Class "fas fa-quote-left fa-border fa-pull-left"] [] ]
+                 str "...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\
+                     Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\
+                     Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.\
+                     Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.." ] ]
 
-// let borderPulledIcons () =
-//     div [ Class "block" ]
-//         [ span [ ]
-//                [ Icon.icon [ ]
-//                              [ Fa.icon Fa.I.QuoteLeft
-//                                Fa.pullLeft
-//                                Fa.border ]
-//                  str "...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\
-//                      Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\
-//                      Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.\
-//                      Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.." ] ]
+let fontAwesomeIcons () =
+    div [ Class "block" ]
+        [ Icon.icon [ Icon.Size IsSmall ] [ i [ Class "fas fa-home" ] [] ]
+          Icon.icon [ ] [ i [ Class "fas fa-tag fa-lg"] [] ]
+          Icon.icon [ Icon.Size IsMedium ] [ i [ Class "fab fa-500px fa-2x"] [] ]
+          Icon.icon [ Icon.Size IsLarge ] [ i [ Class "fab fa-android fa-3x"] [] ] ]
 
-// let fontAwesomeIcons () =
-//     div [ Class "block" ]
-//         [ Icon.icon [ Icon.Size IsSmall ] [ i [ Class "fas fa-home" ] [] ]
-//           Icon.icon [ ] [ Fa.icon Fa.I.Tags; Fa.faLg ]
-//           Icon.icon [ Icon.Size IsMedium ] [ Fa.icon Fa.I.``500px``; Fa.fa2x ]
-//           Icon.icon [ Icon.Size IsLarge ] [ Fa.icon Fa.I.Android; Fa.fa3x ] ]
-
-// let composeButtons () =
-//     div [ Class "block" ]
-//         [ Button.button [ Button.Color IsDanger ]
-//                         [ Icon.icon [ ]
-//                                       [ Fa.icon Fa.I.Trash; Fa.faLg ]
-//                           span [] [ str "  Delete" ] ]
-//           Button.button [ Button.Color IsInfo ]
-//                         [ Icon.icon [ ]
-//                                       [ Fa.icon Fa.I.User; Fa.fw ]
-//                           span [] [ str "User" ] ] ]
+let composeButtons () =
+    div [ Class "block" ]
+        [ Button.button [ Button.Color IsDanger ]
+                        [ Icon.icon [ ]
+                            [ i [ Class "fas fa-trash fa-lg"] [] ]
+                          span [] [ str "  Delete" ] ]
+          Button.button [ Button.Color IsInfo ]
+                        [ Icon.icon [ ]
+                            [ i [ Class "fas fa-user fa-fw"] [] ]
+                          span [] [ str "User" ] ] ]
 
 div [] [
-    Card.card [] [Card.content [] [icon()] ]
     Card.card [] [Card.content [] [containerSizes()] ]
-    // Card.card [] [Card.content [] [iconRotationFlip()] ]
-    // Card.card [] [Card.content [] [iconAnimations()] ]
-    // Card.card [] [Card.content [] [stackedIcons()] ]
-    // Card.card [] [Card.content [] [iconList()] ]
-    // Card.card [] [Card.content [] [borderPulledIcons()] ]
-    // Card.card [] [Card.content [] [fontAwesomeIcons()] ]
-    // Card.card [] [Card.content [] [composeButtons()] ]
+    Card.card [] [Card.content [] [iconRotationFlip()] ]
+    Card.card [] [Card.content [] [iconAnimations()] ]
+    Card.card [] [Card.content [] [stackedIcons()] ]
+    Card.card [] [Card.content [] [iconList()] ]
+    Card.card [] [Card.content [] [borderPulledIcons()] ]
+    Card.card [] [Card.content[] [fontAwesomeIcons()] ]
+    Card.card [] [Card.content [] [composeButtons()] ]
 ] |> mountById "elmish-app"
