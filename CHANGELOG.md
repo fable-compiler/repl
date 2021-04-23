3a3f9ff8b3a2fc92fbd38f0e39e3c7563dfe28a0
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-04-23 11:56:19 +0900

    Release version 3.1.15

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 3.1.15
+
+* Update Fable
+* Add sample: Undertone - Programmable music. @robertpi
+
 ## 3.1.12
 
 * Update Fable
--- src/App/Prelude.fs
+++ src/App/Prelude.fs
@@ -6,7 +6,7 @@ open Fable.Core
 
 [<RequireQualifiedAccess>]
 module Literals =
-    let [<Literal>] REPL_VERSION = "3.1.12" // Do not edit manually
+    let [<Literal>] REPL_VERSION = "3.1.15" // Do not edit manually
     let [<Literal>] STORAGE_KEY = "fable-repl"
     let [<Literal>] MAX_LOGS_LENGTH = 200
     // let [<Literal>] HOST =
