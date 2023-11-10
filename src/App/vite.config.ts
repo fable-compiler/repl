a4d3b7052ad60ab74492aa2ff25c0974db49e85b
Maxime Mangel <mangel.maxime@protonmail.com>
2023-11-10 23:40:11 +0100

    Fix baseUrl when building for production

--- src/App/vite.config.ts
+++ src/App/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-    clearScreen: false
-});
+export default defineConfig(({ command, mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    return {
+        plugins: [react()],
+        clearScreen: false,
+        base: mode === 'production' ? '/repl/' : '/',
+    }
+  })
